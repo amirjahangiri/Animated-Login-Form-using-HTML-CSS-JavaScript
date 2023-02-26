b8ed5ea5faa7ff586405a7da5dc6a989957e033d
Amir Jahangiri <36219742+amirjahangiri@users.noreply.github.com>
2023-02-26 21:15:46 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+         var input = document.querySelector('.pswrd');
+         var show = document.querySelector('.show');
+         show.addEventListener('click', active);
+         function active(){
+           if(input.type === "password"){
+             input.type = "text";
+             show.style.color = "#1DA1F2";
+             show.textContent = "HIDE";
+           }else{
+             input.type = "password";
+             show.textContent = "SHOW";
+             show.style.color = "#111";
+           }
+         }
